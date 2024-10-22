1a404b58c334249600ae493b14ddae28d4a55323
foxxiee04 <sangtranquocgl@gmail.com>
2024-10-22 17:44:49 +0700

    Cho My sua noi dung

---
+++ JS/home.js
@@ -0,0 +1,7 @@
+let currentIndex = 0;
+const images = document.querySelectorAll('.banner img');
+setInterval(() => {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].classList.add('active');
+}, 3000);
